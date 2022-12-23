3c29fea8f81c80473ec6a06df8c9ab8784e84c41
onekill0503 <gamemasteraji123@gmail.com>
2022-12-23 23:58:43 +0700

    test install golang

--- src/utils/terminal.ts
+++ src/utils/terminal.ts
@@ -1,18 +1,26 @@
-import { spawn } from 'child_process'
+import { rejects } from 'assert'
+import { SpawnSyncReturns, spawnSync } from 'child_process'
+import { resolve } from 'path'
 
-export const run = (cmd : string) : void => {
-    const cmdArgs: Array<string> = cmd.split(" ");
-    const mainCmd: string = cmd.split(" ")[0];
-    cmdArgs.shift();
-    let child = spawn(mainCmd , cmdArgs);
-    child.stdout.setEncoding('utf8');
-    child.stderr.setEncoding('utf8');
-    child.stdout.on('data', function(data) {
-        console.log(data);
-    });
-    child.stderr.on('data', function(data) {
-        console.log(data);
-    });
-    child.on('close', function(code) {
-    });
+export const run = async (cmd : string) : Promise<[string , boolean]> => {
+    return await new Promise(async (resolve , reject) => {
+        if(cmd.length > 0){
+            // get command argument
+            const cmdArgs: Array<string> = cmd.split(" ");
+            // get main command
+            const mainCmd: string = cmd.split(" ")[0];
+            cmdArgs.shift();
+            let opt : SpawnSyncReturns<string | Buffer> = await spawnSync(mainCmd , cmdArgs , { encoding: 'utf-8' , shell:true})
+            // print output from command
+            console.log(opt.stdout);
+            console.log(opt.stderr);
+            // if got error return false
+            if(opt.error) resolve([`Successfully install golang`,false]);
+            // if not return true
+            resolve([`Successfully install golang`,true]);
+        }else {
+            // got empty string , come to this.
+            reject(`Got empty string`)
+        }
+    })
 }
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,6 +1,7 @@
 import { SpawnSyncReturns , spawnSync } from "child_process";
 import {  compareVersions , validate } from 'compare-versions'
 import { run as terminal } from './terminal'
+import { createSpinner as spinner } from "nanospinner";
 
 export const isDepedencyInstalled = async (version: boolean | string = false): Promise<boolean> => {
     return await new Promise(async (resolve , reject) => {
@@ -10,7 +11,7 @@ export const isDepedencyInstalled = async (version: boolean | string = false): P
             opt = await spawnSync('go' , [] , {encoding: 'utf-8' , shell: true})
             isFail = (opt?.stderr?.includes(`is not recognized as an internal or external command`) || opt?.stdout?.includes(`command not found`)) ? true : false
         }else{
-            opt= await spawnSync('which' , ['go'] , {encoding: 'utf-8' , shell: true})
+            opt= await spawnSync('which' , ['gos'] , {encoding: 'utf-8' , shell: true})
             isFail = (opt?.stdout == '');
         }
         if(version && !isFail){
@@ -35,11 +36,32 @@ export const installGo = async (version: string = "1.9"): Promise<void> => {
     if(!validate(version)){
         throw new Error(`Invalid Version`);
     }
+    // create loading spinner
+    let spin = spinner(`Downloading golang version ${version}...`).start();
     terminal(`wget https://golang.org/dl/go${version}.linux-amd64.tar.gz`)
+        .then(([res , err]) => {
+            if(!err) throw new Error(res)
+        })
+    spin.update({text:"Remove current golang..."})
     terminal(`sudo rm -rf /usr/local/go`)
+        .then(([res , err]) => {
+            if(!err) throw new Error(res)
+        })
+    spin.update({text:"Installing downloaded golang..."})
     terminal(`sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"`)
+        .then(([res , err]) => {
+            if(!err) throw new Error(res)
+        })
+    spin.update({text:"Remove downloaded golang..."})
     terminal(`rm "go${version}.linux-amd64.tar.gz"`)
+        .then(([res , err]) => {
+            if(!err) throw new Error(res)
+        })
+    spin.update({text:"Extract downloaded golang..."})
     terminal(`echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile`)
-    terminal(`source $HOME/.bash_profile`)
-    terminal(`go version`)
+        .then(([res , err]) => {
+            if(!err) throw new Error(res)
+        })
+    spin.success({text: `Successfully installing golang version ${version}\nYou should restart your terminal.`})
+    
 }
